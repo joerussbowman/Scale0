843f474d684bc4ee811e4d81cb368ff10709e0c6
Joe Bowman <bowman.joseph@gmail.com>
2011-05-14 16:11:00 -0400

    maybe a lesson learned if I do a blog post. zmq.POLLIN only carries on to the next loop when a connection has been made. Pretty sweet actually. Anyway, I've dumbed it way down, right now worker PINGs broker, router multiprocess concept is completely gone, for now. For some reason it appeared to be breaking polling. I think I just need to build a little slower. So concentrating on ping/pong now. Client down to Worker will be next, and then I'll try to plug the router objects in next. Honestly, I think I'm battling Python right now, the GIL sucks. Thinking with something like erlang I could thread without traditional threading resource consumption and still take advtange of multiple processors

--- playground/scale0.py
+++ playground/scale0.py
@@ -21,53 +21,6 @@ import zmq
 import uuid
 import tnetstrings
 from zmq import devices
-from  multiprocessing import Process
-
-class Worker():
-    """
-    Normally these would be separate programs, these are included for testing
-    purposes right now. The goal is that this could eventually grow into a
-    class that others can use in their applications.
-
-    Not really focused on that now. Right now just trying things out to see
-    how they work.
-    """
-    def __init__(self, connect_to):
-        """ The worker connects to a socket to communicate with the Dispatcher
-        in the Broker. This allows the Dispatcher to manage it's LRU queue using
-        the worker. A listener socket is instatiated. This is the socket that the
-        Router in the Broker will make requests to. 
-        """
-        self.my_id = str(uuid.uuid4())
-        self.context = zmq.Context()
-        self.broker_socket = context.socket(zmq.REQ)
-        self.listener_socket = context.socket(zmq.XREP)
-
-        self.broker_socket.setsockopt(zmq.IDENTITY, self.zmq_id)
-        self.broker_socket.connect(connect_to)
-
-        poller = zmq.Poller()
-        poller.register(broker_socket, zmq.POLLIN)
-        poller.register(listener_socket, zmq.POLLIN)
-
-        while True:
-            sock = dict(poller.poll())
-
-            if sock.get(broker_socket) == zmq.POLLIN:
-                multi_message = work_receiver.recv_multipart()
-                message = json.loads(multi_message[1])
-                # This worker just adds a reply with the same content
-                # as the request. This way we can verify the replies
-                # are matching.
-                #
-                #TODO: pretty sure I need to reassemble the request message
-                message["reply"] = "%s" % (message["request"])
-
-                self.listener_socket.send_json(message)
-
-
-      # sender, conn_id, service, body = msg.split(' ', 3)
-
 
 class Dispatcher():
     """ The Dispatcher will accept requests on the client_socket,
@@ -92,125 +45,47 @@ class Dispatcher():
 
         self.my_id = my_id
 
-        # LRU Queue would look something like
-        # [
-        # {"connection": "tcp://127.0.0.1:55555", "services": ["web"]}
-        # {"connection": "tcp://127.0.0.1:55556", "services": ["web"]}
-        # {"connection": "tcp://127.0.0.1:44444", "services": ["news", "mail"]}
-        # ]
-        # Eventually I'll move it to an object with getter and setters which
-        # can use something like gaeutilities event to notify the main
-        # application when a worker is added. That way requests don't
-        # get dropped.
+        """ LRU Queue would look something like
+        [
+        {"connection": "tcp://127.0.0.1:55555", "services": ["web"]}
+        {"connection": "tcp://127.0.0.1:55556", "services": ["web"]}
+        {"connection": "tcp://127.0.0.1:44444", "services": ["news", "mail"]}
+        ]
+        Eventually I'll move it to an object with getter and setters which
+        can use something like gaeutilities event to notify the main
+        application when a worker is added. That way requests don't
+        get dropped. 
+        """
+
         self.LRU = []
 
         self.context = zmq.Context()
 
-        self.client_socket = self.context.socket(zmq.XREP)
         self.worker_socket = self.context.socket(zmq.XREP)
-        self.dispatcher_socket = self.context.socket(zmq.PUSH)  
-        self.router_response_socket = self.context.socket(zmq.SUB)   
-
-        self.client_socket.setsockopt(zmq.IDENTITY, "%s-client" % self.my_id)
         self.worker_socket.setsockopt(zmq.IDENTITY, "%s-worker" % self.my_id)
-        self.dispatcher_socket.setsockopt(zmq.IDENTITY, 
-                "%s-dispatcher" % self.my_id)
-        self.router_response_socket.setsockopt(zmq.IDENTITY, 
-                "%s-router" % self.my_id)
-
-        self.client_socket.bind(client_socket_uri)
         self.worker_socket.bind(worker_socket_uri)
-        self.dispatcher_uri = "%s/%s_dispatcher" % (dispatcher_socket_base, 
-            self.my_id)
-        self.dispatcher_socket.bind(self.dispatcher_uri)
-        self.router_response_uri = "%s/%s_router_response" % (
-            router_response_socket_base, self.my_id)
-        self.router_response_socket.bind(self.router_response_uri)
 
         poller = zmq.Poller()
         poller.register(self.worker_socket, zmq.POLLIN)
-        poller.register(self.client_socket, zmq.POLLIN)
-
-        # get the routers started before we start the loop
-        print 'Starting %s routers' % routers
-
-        router = 0
-        while (router < routers):
-            Process(target=Router, args=(self.context, 
-                self.dispatcher_uri,
-                self.router_response_uri)).start()
-            router = router + 1
-
-        print "Routers started, starting Dispatcher loop"
 
         while True:
-            print "In main Dispatcher loop"
             sock = dict(poller.poll())
-            print 'poll' 
+            print 'poll'
 
-#            if sock.get(self.worker_socket) == zmq.POLLIN:
-#                print "Worker connection"
+            if sock.get(self.worker_socket) == zmq.POLLIN:
+                print "Worker connection"
                 # from the worker we are expecting a multipart message,
                 # part0 = protocol command, part1 = request
-#                message_parts = worker_socket.recv_multipart()
-#                (command, request) = message_parts
-#                if command.upper() == "PING":
-#                    # PING is "uri services time"
-#                    # services are comma delimited
-#                    (uri, services, time) = request.split(" ", 3)
-#                    # TODO: validate time here
-#                    self.LRU.append({"connection": uri, 
-#                        "services": services.split(",")})
-#                    print "Worker %s PING" % uri
-#
-#            if sock.get(self.client_socket) == zmq.POLLIN:
-#                # from the client we are expecting a multipart message
-#                # part0 = service, part1 = request
-#                (service, request) = client_socket.recv_multipart()
-#                # (service, request) = message_parts
-#                connection = None
-#                for i in LRU:
-#                    if service in i["services"]:
-#                        connection = i["connection"]
-#                        LRU.remove(i)
-#                        break
-#                # TODO: Here would be where to plug in something
-#                # if conn == None so that new listeners can be checked for
-#                dispatcher_socket.send_multipart([connection, 
-#                    "%s %s" % (service, request)])
-
-            print 'end loop cycle'
-
-class Router():
-    """ stub of how I think it will work, stopping this and going
-    to just manage everything with the dispatcher at first. This is getting a
-    bit complicated and I want to stop and do one thing at a time"""
-
-    def __init__(self, context, 
-            dispatcher_socket_uri,
-            router_response_socket_uri,
-            my_id=str(uuid.uuid4())):
-
-        self.context = context
-        self.my_id = my_id
-
-        self.dispatcher_socket = context.socket(zmq.PULL)
-        self.dispatcher_socket.setsockopt(zmq.IDENTITY, "%s-router" % self.my_id)
-        self.dispatcher_socket.connect(dispatcher_socket_uri)
-
-        self.response_socket = context.socket(zmq.XREP)
-        self.dispatcher_socket.setsockopt(zmq.IDENTITY, 
-            "%s-router-response" % self.my_id)
-
-        poller = zmq.Poller()
-        poller.register(self.dispatcher_socket, zmq.POLLIN)
-
-        while True:
-            sock = dict(poller.poll())
-
-            if sock.get(dispatcher_socket) == zmq.POLLIN:
-                (connection, request) = work_receiver.recv_multipart()
-                self.context.socket(zmq.REQ).connect(connection).send(request)
+                message_parts = self.worker_socket.recv_multipart()
+                (worker_id, command, request) = message_parts
+                if command.upper() == "PING":
+                    # PING is "uri services time"
+                    # services are comma delimited
+                    (uri, services, time) = request.split(" ", 3)
+                    # TODO: validate time here
+                    self.LRU.append({"connection": uri, 
+                        "services": services.split(",")})
+                    print "Worker %s PING" % uri
 
 if __name__ == "__main__":
     Dispatcher()
--- playground/test_worker.py
+++ playground/test_worker.py
@@ -6,7 +6,7 @@ import tnetstrings
 from zmq import devices
 
 class Worker():
-    def __init__(self, connect_to):
+    def __init__(self, connect_to, listen_on="tcp://127.0.0.1:9080"):
         """ The worker connects to a socket to communicate with the Dispatcher
         in the Broker. This allows the Dispatcher to manage it's LRU queue using
         the worker. A listener socket is instatiated. This is the socket that the
@@ -14,11 +14,15 @@ class Worker():
         """
         self.my_id = str(uuid.uuid4())
         self.context = zmq.Context()
-        self.broker_socket = self.context.socket(zmq.REQ)
-        self.listener_socket = self.context.socket(zmq.XREP)
+        self.listen_on = listen_on
 
-        self.broker_socket.setsockopt(zmq.IDENTITY, self.my_id)
+        self.broker_socket = self.context.socket(zmq.XREQ)
+        self.broker_socket.setsockopt(zmq.IDENTITY, "broker-%s" % self.my_id)
         self.broker_socket.connect(connect_to)
+
+        self.listener_socket = self.context.socket(zmq.XREP)
+        self.listener_socket.setsockopt(zmq.IDENTITY, "listener-%s" % self.my_id)
+        self.listener_socket.bind(self.listen_on)
 
         poller = zmq.Poller()
         poller.register(self.broker_socket, zmq.POLLIN)
@@ -40,7 +44,7 @@ class Worker():
     def connect(self):
         if not self.connected:
             self.broker_socket.send_multipart(["PING", 
-                "test %s" % calendar.timegm(time.gmtime())])
+                "%s test %s" % (self.listen_on, calendar.timegm(time.gmtime()))])
             self.connected = True
 
 if __name__ == "__main__":
